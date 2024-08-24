f502fb675b13fa93c67b3523b3b8b8288a97a2c7
黃柏翰 <huangpohan@pohans-MacBook-Pro.local>
2024-08-24 11:07:25 +0800

    anblama

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,37 +16,45 @@ const IndexPage = () => (
     <ul className={styles.list}>
       <li className={styles.listItem}>
         <Link className={styles.listItemLink} to="/buildings">建筑</Link>
+        <p>完成度：<font color="#FFC300">頁面創建了，但基本沒內容</font></p>
         <p className={styles.listItemDescription}>升级各类建筑，以提升游戏体验和战力。</p>
       </li>
       <li className={styles.listItem}>
         <Link className={styles.listItemLink} to="/resources">资源</Link>
+        <p>完成度：<font color="#32CD32">基本內容完成了，詳細內容之後補充</font></p>
         <p className={styles.listItemDescription}>获取和管理资源的技巧和策略。</p>
       </li>
       <li className={styles.listItem}>
         <Link className={styles.listItemLink} to="/ships">船舰信息</Link>
+        <p>完成度：<font color="#32CD32">基本內容完成了，詳細內容之後補充</font></p>
         <p className={styles.listItemDescription}>获取关于各类船舰的详细资料与最佳使用策略。</p>
       </li>
       <li className={styles.listItem}>
-        <Link className={styles.listItemLink} to="/campaigns">戰役與王牌出擊</Link>
+        <Link className={styles.listItemLink} to="/campaigns">戰役 王牌出擊 競技場</Link>
+        <p>完成度：<font color="#FF0000">頁面尚未創建</font></p>
         <p className={styles.listItemDescription}>遊戲中获取旗舰蓝图和重要资源的关键挑战</p>
       </li>
     </ul>
     <p>以下是另外四個遊玩內容，雖然沒有被歸類主要的部分但也很重要：</p>
     <ul className={styles.list}>
       <li className={styles.listItem}>
-        <Link className={styles.listItemLink} to="/buildings">聯盟</Link>
+        <Link className={styles.listItemLink} to="/alli">聯盟</Link>
+        <p>完成度：<font color="#FF0000">頁面尚未創建</font></p>
         <p className={styles.listItemDescription}>加入和管理联盟，与其他玩家协作，增强整体实力。</p>
       </li>
       <li className={styles.listItem}>
-        <Link className={styles.listItemLink} to="/resources">成員</Link>
+        <Link className={styles.listItemLink} to="/mem">成員</Link>
+        <p>完成度：<font color="#FF0000">頁面尚未創建</font></p>
         <p className={styles.listItemDescription}>分配和升級乘员，以提高舰队的战斗效率。</p>
       </li>
       <li className={styles.listItem}>
-        <Link className={styles.listItemLink} to="/ships">商店</Link>
+        <Link className={styles.listItemLink} to="/shop">商店</Link>
+        <p>完成度：<font color="#FF0000">頁面尚未創建</font></p>
         <p className={styles.listItemDescription}>购买和兑换道具资源，强化游戏中的装备。</p>
       </li>
       <li className={styles.listItem}>
-        <Link className={styles.listItemLink} to="/ships">加成</Link>
+        <Link className={styles.listItemLink} to="/plus">加成</Link>
+        <p>完成度：<font color="#FF0000">頁面尚未創建</font></p>
         <p className={styles.listItemDescription}>获取并应用各种加成效果，提升游戏角色和舰队的属性。</p>
       </li>
     </ul>
